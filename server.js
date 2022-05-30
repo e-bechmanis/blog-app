38533fd22c1a6745c57a9a0dd054a77405853b1e
e-bechmanis <ebechmanis@myseneca.ca>
2022-05-29 23:12:03 -0400

    modifiedcategories/posts  json files

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var express = require("express");
+const path = require('path');
+var express = require('express');
 var app = express();
 
 var HTTP_PORT = process.env.PORT || 8080;
@@ -12,17 +13,13 @@ app.use(express.static('public'));
 
 // Redirects '/' route to '/about' route
 app.get('/', (req,res) => {
-    res.redirect(301, '/about');
-})
+    res.redirect('/about');
+});
 
 // Returns HTML "About" form
 app.get('/about', (req,res) => {
-    res.sendFile(path.join(__dirname,'/views/about.html'));
+    res.sendFile(path.join(__dirname + "/views/about.html"));
 });
-
-//const server = await app.listen(3000);
-//const res = await axios.get('http://localhost:3000/');
-//res.data;
 
 // setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT, onHttpStart);
