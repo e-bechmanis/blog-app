38581dd65eb7ce9ba1c4d846217a47c84dee4d9e
e-bechmanis <ebechmanis@myseneca.ca>
2022-06-11 23:00:37 -0400

    installed dependancies, added new post route

--- server.js
+++ server.js
@@ -16,8 +16,19 @@ const app = express();
 
 const path = require('path');
 const blog = require('./blog-service.js');
+const multer = require("multer");
+const cloudinary = require('cloudinary').v2;
+const streamifier = require('streamifier');
+const upload = multer();
 
 var HTTP_PORT = process.env.PORT || 8080;
+
+cloudinary.config({
+    cloud_name: 'e-bechmanis',
+    api_key: '123182823811362',
+    api_secret: 'YZ1Nu29YsqoRmPnfWYH3h7ayYB8',
+    secure: true
+    });    
 
 // this function will be called after the http server starts listening for requests
 function onHttpStart() {
@@ -33,12 +44,12 @@ app.get('/', (req,res) => {
 
 // Returns HTML "About" form
 app.get('/about', (req,res) => {
-    res.sendFile(path.join(__dirname, '/views/addPost.html'));
+    res.sendFile(path.join(__dirname, '/views/about.html'));
 });
 
 // Returns HTML "Add post" form
 app.get('/posts/add', (req,res) => {
-    res.sendFile(path.join(__dirname, '/views/about.html'));
+    res.sendFile(path.join(__dirname, '/views/addPost.html'));
 });
 
 // Returns a JSON formatted string containing all of the posts where published == true
@@ -60,6 +71,44 @@ app.get('/categories', (req,res) => {
     blog.getCategories()
     .then((data)=>res.json(data))
     .catch((error) => console.log("message: " + error));
+});
+
+app.post('/posts/add', (req,res) => {
+    if(req.file){
+        let streamUpload = (req) => {
+            return new Promise((resolve, reject) => {
+                let stream = cloudinary.uploader.upload_stream(
+                    (error, result) => {
+                        if (result) {
+                            resolve(result);
+                        } else {
+                            reject(error);
+                        }
+                    }
+                );
+    
+    streamifier.createReadStream(req.file.buffer).pipe(stream);
+            });
+        };
+    
+        async function upload(req) {
+            let result = await streamUpload(req);
+            console.log(result);
+            return result;
+        }
+    
+        upload(req).then((uploaded)=>{
+    processPost(uploaded.url);
+        });
+    }else{
+    processPost("");
+    }
+    
+    function processPost(imageUrl){
+    req.body.featureImage = imageUrl;
+    
+        // TODO: Process the req.body and add it as a new Blog Post before redirecting to /posts
+    }     
 });
 
 //The 404 Route
