05712dbce93e7aa2a8d745802a214aff33fabb15
e-bechmanis <ebechmanis@myseneca.ca>
2022-06-20 21:57:17 -0400

    updated heroku link

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 /*********************************************************************************
-*  WEB322 – Assignment 02
+*  WEB322 – Assignment 03
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
 *  Name: Elena Bechmanis     Student ID: 165090218      Date: 19.06.2022
 *
-*  Online (Heroku) URL: https://still-woodland-36555.herokuapp.com/
+*  Online (Heroku) URL: https://fathomless-ocean-19793.herokuapp.com/
 *
 *  GitHub Repository URL: https://github.com/e-bechmanis/web322-app
 *
