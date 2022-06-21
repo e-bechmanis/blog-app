6048e22d5c1ae46e54b080b63fe6fc813157a514
e-bechmanis <ebechmanis@myseneca.ca>
2022-06-20 21:51:32 -0400

    Added filters to post route

--- blog-service.js
+++ blog-service.js
@@ -5,8 +5,8 @@ const { resolve } = require("path");
 let posts = new Array();
 let categories = new Array();
 
-module.exports.initialize = function(){
-    return new Promise(function(resolve, reject){
+module.exports.initialize = () => {
+    return new Promise((resolve, reject) => {
     fs.readFile('./data/posts.json', 'utf8', (err, data) => {
         if (err) {
             reject("Could not open the first file");
@@ -27,16 +27,16 @@ module.exports.initialize = function(){
 })
 }
 
-module.exports.getAllPosts = function(){
-    return new Promise(function(resolve, reject){
+module.exports.getAllPosts = () => {
+    return new Promise((resolve, reject) => {
         if (posts.length > 0) { resolve(posts); }
         else reject("No results returned");
     });
 }
 
-module.exports.getPublishedPosts = function(){
+module.exports.getPublishedPosts = () => {
     let publishedPosts = new Array();
-    return new Promise(function(resolve, reject){
+    return new Promise((resolve, reject) => {
         posts.forEach(element => {
             if (element.published == true) publishedPosts.push(element);})
         if (publishedPosts.length > 0) { resolve(publishedPosts); }
@@ -44,15 +44,15 @@ module.exports.getPublishedPosts = function(){
     });
 }
 
-module.exports.getCategories = function(){
-    return new Promise(function(resolve, reject){
+module.exports.getCategories = () => {
+    return new Promise((resolve, reject) => {
         if (categories.length > 0) { resolve(categories); }
         else reject("No results returned");
     });
 }
 
-module.exports.addPost = function(postData){
-    return new Promise(function(resolve, reject){
+module.exports.addPost = (postData) => {
+    return new Promise((resolve, reject) => {
         postData.published = postData.published === undefined ? false : true;
         postData.id = posts.length + 1;
         posts.push(postData);
@@ -60,8 +60,8 @@ module.exports.addPost = function(postData){
     });
 }
 
-module.exports.getPostsByCategory = function(category){
-    return new Promise(function(resolve,reject){
+module.exports.getPostsByCategory = (category) => {
+    return new Promise((resolve,reject) => {
         let sortedByCategory = new Array();
         posts.forEach(element => {
             if (element.category == category) sortedByCategory.push(element);})
@@ -70,7 +70,7 @@ module.exports.getPostsByCategory = function(category){
 }
 
 module.exports.getPostsByMinDate = function(minDateStr){
-    return new Promise(function(resolve,reject){
+    return new Promise((resolve,reject) => {
         let sortedByDate = new Array();
         posts.forEach(element => {
             if(new Date(element.postDate) >= new Date(minDateStr)) sortedByDate.push(element);})
@@ -78,8 +78,8 @@ module.exports.getPostsByMinDate = function(minDateStr){
     });
 }
 
-module.exports.getPostById = function(id){
-    return new Promise(function(resolve,reject){
+module.exports.getPostById = (id) => {
+    return new Promise((resolve,reject) => {
         let sortedById = new Array();
         posts.forEach(element => {
             if (element.id == id) sortedById.push(element);})
--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Elena Bechmanis     Student ID: 165090218      Date: 12.06.2022
+*  Name: Elena Bechmanis     Student ID: 165090218      Date: 19.06.2022
 *
 *  Online (Heroku) URL: https://still-woodland-36555.herokuapp.com/
 *
@@ -56,34 +56,49 @@ app.get('/posts/add', (req,res) => {
 app.get('/blog', (req,res) => {
     blog.getPublishedPosts()
     .then((data)=>res.json(data))
-    .catch((error) => console.log("message: " + error));
+    .catch((error) => res.json({message: error}));
 });
 
-// Returns a JSON formatted string containing all the posts within the posts.json file
-app.get('/posts', (req,res) => {
-    if (req.query.category >= 1 || req.query.category <=5){
-        blog.getPostsByCategory(req.query.category)
-        .then((data)=>res.json(data))
-        .catch((error) => console.log({message: error}));
-    }
-    else if(){
+//Returns a single post by ID
+app.get('/post/:value', (req,res) => {
+   blog.getPostById(req.params.value).then((data) =>{
+        res.json(data)
+    }).catch((error) => {
+        res.json({message: error})
+    });
+});
 
+//Returns blog posts in JSON format with optional filters by date and category
+app.get('/posts', (req,res) => {
+    if(req.query.category){ //optional filter, returns posts by category
+        blog.getPostsByCategory(req.query.category).then((data) => {
+            res.json(data)
+        }).catch((error) => {
+            res.json({message: error})
+        });
+    }
+    else if (req.query.minDate){ //optional filter, returns posts newer than the date passed in a query
+        blog.getPostsByMinDate(req.query.minDate).then((data) =>{
+            res.json(data)
+        }).catch((error) => {
+            res.json({message: error})
+        });
     }
     else{
         blog.getAllPosts()
-    }
     .then((data)=>res.json(data))
-    .catch((error) => console.log("message: " + error));
+    .catch((error) => res.json({message: error}));
+    }
 });
 
 // Returns a JSON formatted string containing all of the categories within the categories.json file
 app.get('/categories', (req,res) => {
     blog.getCategories()
     .then((data)=>res.json(data))
-    .catch((error) => console.log("message: " + error));
+    .catch((error) => res.json({message: error}));
 });
 
-app.post('/posts/add', (req,res) => {
+app.post('/posts/add', upload.single("featureImage"), (req,res) => {
     if(req.file){
         let streamUpload = (req) => {
             return new Promise((resolve, reject) => {
@@ -130,4 +145,4 @@ app.get('*', (req, res) => {
 // setup http server to listen on HTTP_PORT
 blog.initialize()
 .then(()=> app.listen(HTTP_PORT, onHttpStart))
-.catch((error)=>console.log(error));
+.catch((error)=>res.json({message: error}));
